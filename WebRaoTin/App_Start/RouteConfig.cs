a7e5bfa66d6aed05d1eedb0147e5b9aa31c433d0
huybaoash <huybaoash@gmail.com>
2021-12-07 20:38:22 +0700

    update Friendly URL for User & Admin

--- WebRaoTin/App_Start/RouteConfig.cs
+++ WebRaoTin/App_Start/RouteConfig.cs
@@ -14,12 +14,260 @@ namespace WebRaoTin
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "GioiThieu",
+                url: "gioi-thieu",
+                defaults: new { controller = "Home", action = "GioiThieu", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "PhanHoi",
+                url: "gop-y-phan-hoi",
+                defaults: new { controller = "Home", action = "PhanHoi", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Xac Nhan Email",
+                url: "xac-nhan-email",
+                defaults: new { controller = "Account", action = "ConfirmEmail", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Quen mat khau",
+                url: "quen-mat-khau",
+                defaults: new { controller = "Account", action = "ForgotPassword", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Lam moi mat khau",
+                url: "lam-moi-mat-khau",
+                defaults: new { controller = "Account", action = "ResetPassword", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Xac nhan lam moi mat khau",
+                url: "xac-nhan-lam-moi-mat-khau",
+                defaults: new { controller = "Account", action = "ResetPasswordConfirmation", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Doi mat khau",
+                url: "doi-mat-khau",
+                defaults: new { controller = "Manage", action = "ChangePassword", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Dang Nhap Bang Ung Dung Khac",
+                url: "dang-nhap-bang-ung-dung-khac",
+                defaults: new { controller = "Account", action = "ExternalLoginCallback", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Dang Nhap",
+                url: "dang-nhap",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Dang Ky",
+                url: "dang-ky",
+                defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin SP",
+                url: "danh-sach-tin-san-pham",
+                defaults: new { controller = "SanPhams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin loai SP",
+                url: "danh-sach-tin-san-pham-loai",
+                defaults: new { controller = "SanPhams", action = "Index_LoaiSP", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+            
+
+            routes.MapRoute(
+                name: "Danh sach tin DV",
+                url: "danh-sach-tin-dich-vu",
+                defaults: new { controller = "DichVus", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin loai DV",
+                url: "danh-sach-tin-dich-vu-loai",
+                defaults: new { controller = "DichVus", action = "Index_LoaiDV", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin BDS",
+                url: "danh-sach-tin-bat-dong-san",
+                defaults: new { controller = "BatDongSans", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin loai BDS",
+                url: "danh-sach-tin-bat-dong-san-loai",
+                defaults: new { controller = "BatDongSans", action = "Index_LoaiBDS", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin VL",
+                url: "danh-sach-tin-viec-lam",
+                defaults: new { controller = "ViecLams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Danh sach tin loai VL",
+                url: "danh-sach-tin-viec-lam-loai",
+                defaults: new { controller = "ViecLams", action = "Index_LoaiVL", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+            routes.MapRoute(
+                name: "Lich su dang tin",
+                url: "{id}-lich-su-dang-tin",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Index_ofUser",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Lich su binh luan",
+                url: "{id}-lich-su-binh-luan",
+                defaults: new
+                {
+                    controller = "BinhLuans",
+                    action = "CMT_ofUser",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+
+            routes.MapRoute(
+                name: "TinTuc Details",
+                url: "{id}-chi-tiet-tin-tuc",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Details",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "TinTuc Edit",
+                url: "{id}-chinh-sua-tin-tuc",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Chỉnh sửa trang cá nhân",
+                url: "{id}-sua-trang-ca-nhan",
+                defaults: new
+                {
+                    controller = "Users",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Trang cá nhân",
+                url: "{id}-trang-ca-nhan",
+                defaults: new
+                {
+                    controller = "Users",
+                    action = "Details",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            
+
+            routes.MapRoute(
+                name: "Đăng tin",
+                url: "dang-tin-tuc",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Create",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Trang chủ",
+                url: "trang-chu",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Controllers"
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 new[] { "WebRaoTin.Controllers"
                 }
             );
+
+            
         }
     }
 }
--- WebRaoTin/Areas/Admin/AdminAreaRegistration.cs
+++ WebRaoTin/Areas/Admin/AdminAreaRegistration.cs
@@ -15,6 +15,363 @@ namespace WebRaoTin.Areas.Admin
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+            
+
+            context.MapRoute(
+                name: "Danh sach tin SP Admin",
+                url: "danh-sach-tin-san-pham-admin",
+                defaults: new { controller = "SanPhams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin loai SP Admin",
+                url: "danh-sach-tin-san-pham-loai-admin",
+                defaults: new { controller = "SanPhams", action = "Index_LoaiSP", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+
+            context.MapRoute(
+                name: "Danh sach tin DV Admin",
+                url: "danh-sach-tin-dich-vu-admin",
+                defaults: new { controller = "DichVus", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin loai DV Admin",
+                url: "danh-sach-tin-dich-vu-loai-admin",
+                defaults: new { controller = "DichVus", action = "Index_LoaiDV", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin BDS Admin",
+                url: "danh-sach-tin-bat-dong-san-admin",
+                defaults: new { controller = "BatDongSans", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin loai BDS Admin",
+                url: "danh-sach-tin-bat-dong-san-loai-admin",
+                defaults: new { controller = "BatDongSans", action = "Index_LoaiBDS", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin VL Admin",
+                url: "danh-sach-tin-viec-lam-admin",
+                defaults: new { controller = "ViecLams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin loai VL Admin",
+                url: "danh-sach-tin-viec-lam-loai-admin",
+                defaults: new { controller = "ViecLams", action = "Index_LoaiVL", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            
+
+            context.MapRoute(
+                name: "TinTuc Edit Admin",
+                url: "{id}-chinh-sua-tin-tuc-admin",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiSP Edit Admin",
+                url: "{id}-chinh-sua-tin-tuc-loai-sp-admin",
+                defaults: new
+                {
+                    controller = "LoaiSanPhams",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiDV Edit Admin",
+                url: "{id}-chinh-sua-tin-tuc-loai-dv-admin",
+                defaults: new
+                {
+                    controller = "LoaiDichVus",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiBDS Edit Admin",
+                url: "{id}-chinh-sua-tin-tuc-loai-bds-admin",
+                defaults: new
+                {
+                    controller = "LoaiBatDongSans",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiVL Edit Admin",
+                url: "{id}-chinh-sua-tin-tuc-loai-vl-admin",
+                defaults: new
+                {
+                    controller = "LoaiViecLams",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Chỉnh sửa trang cá nhân Admin",
+                url: "{id}-sua-trang-ca-nhan-admin",
+                defaults: new
+                {
+                    controller = "Users",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Trang cá nhân Admin",
+                url: "{id}-trang-ca-nhan-admin",
+                defaults: new
+                {
+                    controller = "Users",
+                    action = "Details",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+
+            context.MapRoute(
+                name: "Lich su dang tin Admin",
+                url: "{id}-lich-su-dang-tin-admin",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Index_ofUser",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Lich su binh luan Admin",
+                url: "{id}-lich-su-binh-luan-admin",
+                defaults: new
+                {
+                    controller = "BinhLuans",
+                    action = "CMT_ofUser",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc Admin Details",
+                url: "{id}-chi-tiet-tin-tuc-admin",
+                defaults: new
+                {
+                    controller = "TinTucs",
+                    action = "Details",
+                    
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiSP Admin Details",
+                url: "{id}-chi-tiet-tin-tuc-loai-sp-admin",
+                defaults: new
+                {
+                    controller = "LoaiSanPhams",
+                    action = "Details",
+
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiDV Admin Details",
+                url: "{id}-chi-tiet-tin-tuc-loai-dv-admin",
+                defaults: new
+                {
+                    controller = "LoaiDichVus",
+                    action = "Details",
+
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiBDS Admin Details",
+                url: "{id}-chi-tiet-tin-tuc-loai-bds-admin",
+                defaults: new
+                {
+                    controller = "LoaiBatDongSans",
+                    action = "Details",
+
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "TinTuc LoaiVL Admin Details",
+                url: "{id}-chi-tiet-tin-tuc-loai-vl-admin",
+                defaults: new
+                {
+                    controller = "LoaiViecLams",
+                    action = "Details",
+
+                    id = UrlParameter.Optional
+                },
+                namespaces: new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach tin tuc Admin",
+                url: "danh-sach-tin-tuc-admin",
+                defaults: new { controller = "TinTucs", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach loai SP Admin",
+                url: "danh-sach-loai-san-pham-admin",
+                defaults: new { controller = "LoaiSanPhams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach loai DV Admin",
+                url: "danh-sach-loai-dich-vu-admin",
+                defaults: new { controller = "LoaiDichVus", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach loai BDS Admin",
+                url: "danh-sach-loai-bat-dong-san-admin",
+                defaults: new { controller = "LoaiBatDongSans", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach loai VL Admin",
+                url: "danh-sach-loai-viec-lam-admin",
+                defaults: new { controller = "LoaiViecLams", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Danh sach nguoi dung Admin",
+                url: "danh-sach-nguoi-dung-admin",
+                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Đăng tin tức Admin",
+                url: "dang-tin-admin",
+                defaults: new { controller = "TinTucs", action = "Create", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Thêm loại SP Admin",
+                url: "them-loai-sp-admin",
+                defaults: new { controller = "LoaiSanPhams", action = "Create", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Thêm loại DV Admin",
+                url: "them-loai-dv-admin",
+                defaults: new { controller = "LoaiDichVus", action = "Create", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Thêm loại BDS Admin",
+                url: "them-loai-bds-admin",
+                defaults: new { controller = "LoaiBatDongSans", action = "Create", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                name: "Thêm loại VL Admin",
+                url: "them-loai-vl-admin",
+                defaults: new { controller = "LoaiViecLams", action = "Create", id = UrlParameter.Optional },
+                new[] { "WebRaoTin.Areas.Admin.Controllers"
+                }
+            );
+
+            context.MapRoute(
+                "Trang chủ Admin",
+                "trang-chu-admin",
+                new
+                {
+                    action = "Index",
+                    controller = "Home",
+                    id = UrlParameter.Optional
+                },
+                 new[] { "WebRaoTin.Areas.Admin.Controllers" }
+            );
+
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
@@ -26,6 +383,8 @@ namespace WebRaoTin.Areas.Admin
                 },
                  new[] { "WebRaoTin.Areas.Admin.Controllers" }
             );
+
+
         }
     }
 }
